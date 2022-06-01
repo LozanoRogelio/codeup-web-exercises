bd3049ddfce28b7592fa4c9f1e2cd785f422ad71
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-06-01 15:32:58 -0500

    Exercise 3 continued

--- js/external.js
+++ js/external.js
@@ -1,1 +1,25 @@
 console.log("Hello from external Javascript")
+
+alert("Welcome to my Website!")
+
+let userInput = prompt("What is you favorite color?");
+if (userInput = "blue") {
+    alert("Great, blue is my favorite color too!")
+}
+
+
+// You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for movie per day is $3, how much will you have to pay?
+let littleMermaid = 3, brotherBear = 5, hercules = 1;
+let pricePerDay = 3;
+let total = ((littleMermaid + brotherBear + hercules) * pricePerDay);
+
+alert("The amount you will pay is $" + total + " to rent all movies")
+
+
+
+// Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+
+// A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+
+// A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
