eb0659f14acc4b7e99750ffdc330ee39ea58ae7e
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-06-07 10:06:08 -0500

    Finished For Loops Exercises

---
+++ js/for_loops.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// Question 2
+function showMultiplicationTable(num) {
+    for (let n = 0; n <= 10; ++n) {
+        let result = num * n
+        console.log("For this multiplication table you got " + num + " * " + n + " = " + result)
+
+    }
+}
+
+// Question 3
+
+// function getRandomNumber(number) {
+//     for (let a = 0; a <= 10; ++a) {
+//         console.log(randomNumber + " is " + (randomNumber % 2 === 0))
+//     }
+//     let randomNumber = Math.floor(Math.random() * (200 -20 + 1)) + 20;
+//
+// }
+
+// Question 4
+// let output = "";
+// for (let b = 1; b <= 9; b++ ) {
+//     for (let c = 1; c <= b; c++) {
+//         output += b + " ";
+//     }
+//     console.log(output);
+//     output="";
+// }
+
+// Question 5
+
+// for (let i = 100; i >= 5; i = i -5) {
+//     console.log(i)
+// }
+
+
+
+
+
