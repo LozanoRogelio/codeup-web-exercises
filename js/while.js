659634004136e8b26a008d7c89fcabf297adfc92
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-06-07 15:59:28 -0500

    Finishing the last Do While Loop

--- js/while.js
+++ js/while.js
@@ -13,4 +13,20 @@
 
 let allCones = Math.floor(Math.random() * 50) + 50;
 
+let t = 1
 
+do {
+    let conesBought = Math.floor(Math.random() * 5) + 1;
+    console.log("A total of " + conesBought + " cones were sold")
+    t++
+} while (t >= 100);
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+
+let t = 1
+
+do {
+    let conesBought = Math.floor(Math.random() * 5) + 1;
+    console.log("A total of " + conesBought + " cones were sold")
+    t++
+} while (t >= 100);
