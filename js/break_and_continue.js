8dd6110ad9adbb8d9cc7b09272d0c70b4ac5fe57
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-06-07 15:23:40 -0500

    While and Do While Loops in Progress

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,14 +1,14 @@
-"use strict";
-
-let userNum = prompt("Choose an odd number between 1 and 50!")
-
-for (let k = 0; k <= 50; k++) {
-    if (k % 2 !== 0) {
-    console.log("Here is an odd number: " + k)
-    } else if (userNum) {
-        console.log("Yikes, skipping number: " + userNum)
-        continue;
-    }
-
-    }
+// "use strict";
+//
+// let userNum = prompt("Choose an odd number between 1 and 50!")
+//
+// for (let k = 0; k <= 50; k++) {
+//     if (k % 2 !== 0) {
+//     console.log("Here is an odd number: " + k)
+//     } else if (userNum) {
+//         console.log("Yikes, skipping number: " + userNum)
+//         continue;
+//     }
+//
+//     }
 
---
+++ js/while.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// While Loop Exercise
+// let r = 2;
+// while (r <= 65536) {
+//     r = (r * 2)-2
+//     console.log(r);
+//     r++;
+//
+// }
+
+// Do While Exercise
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+
+
