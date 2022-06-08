a04f3048fd07101b4b34959558c863f1157c05a8
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-06-08 13:54:42 -0500

    Iterating Exercises down to the last ToDo Question

--- js/while.js
+++ js/while.js
@@ -11,29 +11,22 @@
 
 // Do While Exercise
 
+
+
 // let allCones = Math.floor(Math.random() * 50) + 50;
-//
+
 // let t = 1
 //
 // do {
 //     let conesBought = Math.floor(Math.random() * 5) + 1;
-//     console.log("A total of " + conesBought + " cones were sold")
-//     t++
-// } while (t >= 100);
+//
+//     if(allCones >= conesBought) {
+//         allCones = allCones - conesBought;
+//         console.log(conesBought + " cones sold! I have " + allCones)
+//     } else {
+//         console.log("Oopss - I only have " + allCones + " left. Sorry I can't sell you "+ conesBought + ".")
+//     }
+// } while (allCones !== 0);
+//
+// console.log("All CONES SOLD!!")
 
-let allCones = Math.floor(Math.random() * 50) + 50;
-
-let t = 1
-
-do {
-    let conesBought = Math.floor(Math.random() * 5) + 1;
-
-    if(allCones >= conesBought) {
-        allCones = allCones - conesBought;
-        console.log(conesBought + " cones sold! I have " + allCones)
-    } else {
-        console.log("Oopss - I only have " + allCones + " left. Sorry I can't sell you "+ conesBought + ".")
-    }
-} while (allCones !== 0);
-
-console.log("All CONES SOLD!!")
