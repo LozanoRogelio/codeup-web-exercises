32efe7174932cb71bd68448d28a3cec9e230ab7f
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-07-18 15:28:59 -0500

    Finished setting divs to appropriate weather descriptions

---
+++ js/codeupquizjs2.js
@@ -0,0 +1,12 @@
+
+if (true) {
+    let x = 5;
+}
+console.log(typeof x);
+
+let numbers = [1, 2, 3, 4, 5];
+numbers.map(n => n * 3)
+console.log(numbers)
+
+const sayHello = (name) => 'Hello, ' + name + '!';
+console.log(sayHello())
