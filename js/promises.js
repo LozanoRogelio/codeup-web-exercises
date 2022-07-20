2fab890bc049a05628529c09de3a58220c3c094e
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-07-20 14:42:11 -0500

    Finished getting latest commit through promises

--- js/promises.js
+++ js/promises.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
         function getGithubUsernames() {
-            return fetch('https://api.github.com/users', options)
+            return fetch(`https://api.github.com/users/LozanoRogelio/events/public`, options)
                 .then(response => response.json())
                 .catch(function(error) {
                     console.warn("ERROR: " + error);
@@ -23,9 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
         const users = await getGithubUsernames()
         console.log(users);
 
-        function getLastCommit() {
-            users.then(response => response.json())
-        };
+        function getLastCommit(username) {
+            return fetch("https://api.github.com/users/" + username + "/events/public", options)
+                .then(res => {
+                    return res.json();
+                }).then(events => {
+                    for(let event of events) {
+                        if (event.type === 'PushEvent') {
+                            return event.payload.commits;
+                        }
+                    }
+                });
+
+        }
+        const lastEvent = await getLastCommit("LozanoRogelio");
+        console.log(lastEvent);
+
 
 
     })();
