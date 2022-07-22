3423eca88a400f1d34317b3bdc7cdd82ea339678
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-07-22 10:13:14 -0500

    Working on quotes function to display them on page

--- jalopy-main/js/router.js
+++ jalopy-main/js/router.js
@@ -9,6 +9,7 @@ import {RegisterEvent} from "./views/Register.js";
 import UserIndex, {UserEvents} from "./views/User.js";
 import Logout, {LogoutEvents} from "./views/Logout.js";
 import DogFactsView, {DogFactsEvents} from "./views/DogFacts.js";
+import QuotesView, {QuotesEvents} from "./views/Quotes.js";
 
 /**
  * Returns the route object for a specific route based on the given URI
@@ -77,6 +78,13 @@ export default function router(URI) {
             uri: '/dogs',
             title: 'Dog Facts',
             viewEvent: DogFactsEvents
+        },
+        '/quotes': {
+            returnView: QuotesView,
+            state: {},
+            uri: '/quotes',
+            title: 'Quotes',
+            viewEvent: QuotesEvents
         },
     };
 
